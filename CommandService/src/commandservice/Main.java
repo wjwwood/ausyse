873787c6e9e5216f3026517fdf61a4f6e2ed6354
Jarrett Chapman <chapmanjt@gmail.com>
2011-04-11 19:57:51 -0500

    Command Service UI for FIFO, LIFO, and Closest

---
+++ CommandService/src/commandservice/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package commandservice;
+
+/**
+ *
+ * @author jaret
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        CommandUI ui = new CommandUI();
+        ui.setVisible(true);
+
+    }
+
+}
